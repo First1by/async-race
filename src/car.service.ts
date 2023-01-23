b1c69b2c48db411d9f36186096c5ebee2a123a30
First1by <greysound1991@mail.ru>
2023-01-23 23:53:20 +0300

    feat: add animation of the movement of cars

--- src/car.service.ts
+++ src/car.service.ts
@@ -1,17 +1,20 @@
 export class CarService {
     getCars(): Promise<any> {
-        return fetch('http://localhost:3000/cars', {
-            method: 'GET'
-        }).then(res => res.json());
-
+        return fetch('http://localhost:3000/garage', {
+            method: 'GET',
+        }).then((res) => res.json());
     }
 
     start(id: number): Promise<any> {
-        return fetch('http://localhost:3000/engine?' + new URLSearchParams({
-             id: id.toString(),
-             status: 'started'
-            }), {
-            method: 'PATCH'
-        }).then(res => res.json());
+        return fetch(
+            'http://localhost:3000/engine?' +
+                new URLSearchParams({
+                    id: id.toString(),
+                    status: 'started',
+                }),
+            {
+                method: 'PATCH',
+            }
+        ).then((res) => res.json());
     }
 }
